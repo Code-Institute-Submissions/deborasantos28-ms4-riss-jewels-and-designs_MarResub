a1d0644802acda02fa10cbea6608db54f9f78f81
deborasantos28 <debora_hardy@hotmail.com>
2022-01-30 21:56:37 +0000

    Deployment to Heroku by connecting the allowed hosts and disabling static

--- ms4_riss_jewels_and_designs/settings.py
+++ ms4_riss_jewels_and_designs/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-mn(r7l6n=7!l2=smus6nc@xsb)=1a_!zp6#m9shb5((q*z5x9&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ms4-riss-jewels-and-designs.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -123,13 +123,17 @@ WSGI_APPLICATION = 'ms4_riss_jewels_and_designs.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
-
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation

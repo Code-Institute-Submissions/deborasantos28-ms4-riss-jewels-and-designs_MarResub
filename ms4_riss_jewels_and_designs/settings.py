1328c734da4c61127ca57508d769cd0ec4a19416
deborasantos28 <debora_hardy@hotmail.com>
2022-01-30 22:12:57 +0000

    Removed Secret Key and and set debug for true only if in development

--- ms4_riss_jewels_and_designs/settings.py
+++ ms4_riss_jewels_and_designs/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-mn(r7l6n=7!l2=smus6nc@xsb)=1a_!zp6#m9shb5((q*z5x9&'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['ms4-riss-jewels-and-designs.herokuapp.com', 'localhost']
 

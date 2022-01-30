ca465fb479d064668374aeb26f36323da2968556
deborasantos28 <debora_hardy@hotmail.com>
2022-01-30 21:41:16 +0000

    Deployment to heroku initiated, set up fixtures and migrations

--- ms4_riss_jewels_and_designs/settings.py
+++ ms4_riss_jewels_and_designs/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -125,9 +126,10 @@ WSGI_APPLICATION = 'ms4_riss_jewels_and_designs.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
 
 
 # Password validation

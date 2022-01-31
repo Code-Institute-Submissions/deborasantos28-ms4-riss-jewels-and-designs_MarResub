cc73230f8bcd4c7f64dc209494c515c8766e5483
deborasantos28 <debora_hardy@hotmail.com>
2022-01-31 20:50:56 +0000

    Attempting deployment to heroku and correcting the login problem

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn boutique_ado.wsgi:application
+web: gunicorn ms4_riss_jewels_and_designs.wsgi:application
--- ms4_riss_jewels_and_designs/settings.py
+++ ms4_riss_jewels_and_designs/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = 'gk6f#^5um9ldtdl$(r7*m)$wtg(#e9vntvlrvrf_sfrel1!ihe'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ms4-riss-jewels-and-designs.herokuapp.com', 'localhost']
+]
 
 
 # Application definition
@@ -123,11 +124,17 @@ WSGI_APPLICATION = 'ms4_riss_jewels_and_designs.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
+else:
+    DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators

510f2bf2003b02803f797fd0fc22a39210ad40af
deborasantos28 <debora_hardy@hotmail.com>
2022-02-06 00:07:55 +0000

    Created a delete_modal for the blog app

---
+++ blog/forms.py
@@ -0,0 +1,51 @@
+from django import forms
+# Imported from products app
+from products.widgets import CustomClearableFileInput
+from .models import Comment, BlogEntry
+
+
+class BlogEntryForm(forms.ModelForm):
+
+    class Meta:
+        model = BlogEntry
+        fields = ('title', 'image', 'content',)
+
+    image = forms.ImageField(label='Image', required=True,
+                             widget=CustomClearableFileInput)
+
+    def __init__(self, *args, **kwargs):
+        """
+        Function sets placeholders, adds auto-focus to top field
+        and assigns all fields a class
+        """
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'title': 'Title',
+            'image': 'Image',
+            'content': 'Blog Content',
+        }
+
+        self.fields['title'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            if field != 'image':
+                self.fields[field].widget.attrs['placeholder'] = (
+                    placeholders[field])
+            self.fields[field].widget.attrs['class'] = 'blog-form-input'
+
+
+class CommentForm(forms.ModelForm):
+
+    class Meta:
+        model = Comment
+        fields = ('body',)
+
+    def __init__(self, *args, **kwargs):
+        """
+        Function sets placeholder, removes auto-generated input label
+        and assigns a class
+        """
+        super().__init__(*args, **kwargs)
+        self.fields['body'].widget.attrs['placeholder'] = (
+            'Write your comment here... (500 characters max).')
+        self.fields['body'].label = False
+        self.fields['body'].widget.attrs['class'] = 'comment-form-input'

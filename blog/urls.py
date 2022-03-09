f9fd3396c47e731b543b5fa9c629e74e121a008a
deborasantos28 <debora_hardy@hotmail.com>
2022-03-09 21:26:44 +0000

    added the urls to connect to the respective html bodies

--- blog/urls.py
+++ blog/urls.py
@@ -1,3 +1,12 @@
 from django.urls import path
 from . import views
 
+urlpatterns = [
+    path('', views.blog_posts, name='blog_posts'),
+    path('add_post/', views.add_blog_post, name='add_blog_post'),
+    path('edit_post/<slug:slug>/', views.edit_blog_post,
+         name='edit_blog_post'),
+    path('delete_post/<slug:slug>/', views.delete_blog_post,
+         name='delete_blog_post'),
+    path('<slug:slug>/', views.blog_detail, name='blog_detail'),
+]
